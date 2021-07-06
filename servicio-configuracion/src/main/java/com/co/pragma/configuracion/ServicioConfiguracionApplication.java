4759b2adb3453b1e98179a05ee8c7a6caa60b31e
Camilo Leguizamon <camilo-leguizamon1@upc.edu.co>
2021-07-06 17:42:02 -0500

    commit test

--- servicio-configuracion/src/main/java/com/co/pragma/configuracion/ServicioConfiguracionApplication.java
+++ servicio-configuracion/src/main/java/com/co/pragma/configuracion/ServicioConfiguracionApplication.java
@@ -2,7 +2,9 @@ package com.co.pragma.configuracion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ServicioConfiguracionApplication {
 
